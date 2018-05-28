a63c548940d0862f21342a5bea3aa9e3ba1dc7a9
Jay <13395097271@163.com>
2018-05-28 17:36:29 +0800

    简单 集成redis

--- springbootdemo2/src/main/java/spring/boot/demo/controller/Democontroller.java
+++ springbootdemo2/src/main/java/spring/boot/demo/controller/Democontroller.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import spring.boot.demo.dao.dto.ActivityRelation;
 import spring.boot.demo.service.ActivityRelationService;
+import spring.boot.demo.service.RedisService;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -18,31 +19,52 @@ import java.util.List;
  * Date 2018/5/23
  */
 @Controller
-public class Democontroller{
+public class Democontroller {
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private ActivityRelationService activityRelationService;
 
+    @Autowired
+    private RedisService redisService;
+
 
     @RequestMapping("/hello")
-    public  String main(Model mv){
-        List lis= new ArrayList();
-        ActivityRelation activityRelation=activityRelationService.findData(144);
+    public String main(Model mv) {
+        List lis = new ArrayList();
+        ActivityRelation activityRelation = activityRelationService.findData(144);
         lis.add(activityRelation.getFormSn());
         lis.add(activityRelation.getParentFormNo());
         lis.add(activityRelation.getParentFormSn());
         lis.add(activityRelation.getFormNo());
-        mv.addAttribute("name",lis);
+        mv.addAttribute("name", lis);
         return "index";
     }
 
+    /**
+     * 日志
+     */
     @RequestMapping("log")
-    public void logTest(){
+    public void logTest() {
         logger.trace("I am trace log.");
         logger.debug("I am debug log.");
         logger.info("I am info log.");
         logger.warn("I am warn log.");
         logger.error("I am error log.{}", new Date());
+    }
+
+    /**
+     * reids测试
+     */
+    @RequestMapping("redis")
+    public String reidsTest(Model mv) {
+        List<Object> list = new ArrayList<>();
+        redisService.set("test:data","redisData");
+        Object tmp = redisService.get("test");
+        logger.info(tmp + "*********************************");
+        System.out.println(tmp);
+        list.add(tmp);
+        mv.addAttribute("name", list);
+        return "index";
     }
 }
