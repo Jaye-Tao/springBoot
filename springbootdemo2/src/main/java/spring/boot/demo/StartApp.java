5daa7bf18d97e73afb95f315cd2c0e5fae863366
Jay <13395097271@163.com>
2018-05-31 10:01:13 +0800

    排除 springboot启动时会自动注入数据源和配置jpa

--- springbootdemo2/src/main/java/spring/boot/demo/StartApp.java
+++ springbootdemo2/src/main/java/spring/boot/demo/StartApp.java
@@ -3,6 +3,8 @@ package spring.boot.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -10,7 +12,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * Created Jay
  * Date 2018/5/23
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @MapperScan("spring.boot.demo.dao.mapper")
 public class StartApp extends SpringBootServletInitializer {
 
@@ -18,15 +20,15 @@ public class StartApp extends SpringBootServletInitializer {
         SpringApplication.run(StartApp.class, args);
     }
 
-    /**
-     * 改为war 部署修改启动类
-     * @param builder
-     * @return
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        // TODO Auto-generated method stub
-//      return super.configure(builder);
-        return builder.sources(this.getClass());
-    }
+//    /**
+//     * 改为war 部署修改启动类
+//     * @param builder
+//     * @return
+//     */
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        // TODO Auto-generated method stub
+////      return super.configure(builder);
+//        return builder.sources(this.getClass());
+//    }
 }
